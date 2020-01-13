ead0c006df629b708b1e4b5e45bfa6416627ab60
Conrado1212 <conrado1212@gmail.com>
2020-01-13 17:18:50 +0100

    Raport

--- src/main/java/pl/edu/wszib/dao/impl/RaportDAOImpl.java
+++ src/main/java/pl/edu/wszib/dao/impl/RaportDAOImpl.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Component;
 import pl.edu.wszib.dao.IRaportDAO;
 import pl.edu.wszib.model.Raport;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -18,26 +20,32 @@ public class RaportDAOImpl implements IRaportDAO {
     private List<Raport> raports = new ArrayList<>();
 
     public RaportDAOImpl(){
+        Date dNow = new Date( );
+        SimpleDateFormat ft =
+                new SimpleDateFormat("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+
+        System.out.println("Current Date: " + ft.format(dNow));
+
         Raport raport1 = new Raport();
         raport1.setLogin("Mateusz");
         raport1.setPointOfSales("point1");
-        raport1.setWhenWasLogged("8:45  11.12.19");
+        raport1.setWhenWasLogged("" + ft.format(dNow));
 
         Raport raport2 = new Raport();
         raport2.setLogin("Piotr");
         raport2.setPointOfSales("point2");
-        raport2.setWhenWasLogged("10:58  17.12.19");
+        raport2.setWhenWasLogged("" + ft.format(dNow));
 
         Raport raport3 = new Raport();
         raport3.setLogin("Paweł");
         raport3.setPointOfSales("point3");
-        raport3.setWhenWasLogged("16:30  21.12.19");
+        raport3.setWhenWasLogged("" + ft.format(dNow));
 
 
         Raport raport4 = new Raport();
         raport4.setLogin("Jan");
         raport4.setPointOfSales("point4");
-        raport4.setWhenWasLogged("14:33  26.12.19");
+        raport4.setWhenWasLogged("" + ft.format(dNow));
 
         this.raports.add(raport1);
         this.raports.add(raport2);
