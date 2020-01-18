7c6bb2705fb78ec7f47621ce4e4804811333fd68
Nazarii <31996309+Holinei@users.noreply.github.com>
2020-01-18 18:45:29 +0100

    added RepositoryPointOfSales and DAO

---
+++ src/main/java/pl/edu/wszib/dao/impl/RepositoryPointOfSalesImpl.java
@@ -0,0 +1,4 @@
+package pl.edu.wszib.dao.impl;
+
+public class RepositoryPointOfSalesImpl {
+}
---
+++ src/main/java/pl/edu/wszib/model/RepositoryPointOfSales.java
@@ -0,0 +1,6 @@
+package pl.edu.wszib.model;
+
+public class ReportPointOfSales {
+    private String idPointOfSales;
+    private String ff;
+}
